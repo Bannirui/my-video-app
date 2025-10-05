a8740060dfde38a30fb0993d6c43a1de49ea57c9
dingrui <Bannirui@outlook.com>
2025-10-05 23:47:04 +0800

    init glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+#include <GLFW/glfw3.h>
+
 #include "Log.h"
 
 int main(int argc, const char *argv[]) {
+    GLFWwindow *window;
+    if (!glfwInit()) {
+        MY_LOG_ERROR("Failed to initialize GLFW");
+        return 1;
+    }
+    window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL);
+    if (!window) {
+        MY_LOG_ERROR("could not create GLFW window");
+        return 1;
+    }
     MY_LOG_INFO("HELLO WORLD!");
     return 0;
 }
